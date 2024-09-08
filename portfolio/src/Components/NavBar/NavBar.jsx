d8a652c31aff69da6f0483c858478b5315801813
AnthonyLaw <anthonylaw910@gmail.com>
2024-09-08 17:56:49 -0600

    navbar

---
+++ portfolio/src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./NavBar.css";
+import logo from "../../assets/logo.png";
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <img src={logo} alt="logo" className="navbar-logo" />
+      <ul className="nav-menu">
+        <li>Home</li>
+        <li>About Me</li>
+        <li>Portfolio</li>
+        <li>Contact</li>
+      </ul>
+      <div className="nav-connect">Connect With Me</div>
+    </div>
+  );
+};
+
+export default NavBar;
