cdfa23276a8202ec94b7588928c6d5827b258b1b
AnthonyLaw <anthonylaw910@gmail.com>
2024-09-08 20:27:11 -0600

    hero component

---
+++ portfolio/src/Components/Hero/Hero.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Hero.css";
+import self from "../../../assets/self_cropped.png";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <img src={self} alt="self" />
+      <h1>
+        <span>Hey I'm Anthony, </span> a full-stack developer with a primary
+        focus in React & Express.
+      </h1>
+      <p>
+        I am a full-stack developer located in Salt Lake, looking to break into
+        tech. Excited to work with you & continue to learn & grow.
+      </p>
+      <div className="hero-action">
+        <div className="hero-connect">Connect with me</div>
+        <div className="hero-resume">My resume</div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
