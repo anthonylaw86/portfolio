9b4b911f2a55d90e7c64a210abedd4d32cb1115d
Anthony Law <anthonylaw910@gmail.com>
2024-11-19 17:06:24 -0700

    changes

--- assets/mywork_data.js
+++ assets/mywork_data.js
@@ -1,6 +1,7 @@
 import beatApp from "./beatApp.1.png";
 import aroundUS from "./aroundTheUS.1.png";
 import wtwr from "./WTWR.2.png";
+import darkDeco from "./darkDeco.png";
 
 const mywork_data = [
   {
@@ -19,6 +20,12 @@ const mywork_data = [
     work_no: 3,
     work_name: "WTWR",
     work_image: wtwr,
+  },
+
+  {
+    work_no: 4,
+    work_name: "Dark Deco E-Commerce",
+    work_image: darkDeco,
   },
 ];
 
